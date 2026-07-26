d2102f4490e4bef332b1c814c2877b3e01f7d175
Vincent Boulaye <vincent.boulaye@orange.com>
2026-07-26 23:02:58 +0200

    prerender

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,4 @@
-export const ssr = false;
-
+export const prerender = 'auto'
 
 //needed for pages deployment to handle callback
 export const trailingSlash = 'always';
--- svelte.config.ts
+++ svelte.config.ts
@@ -3,7 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      fallback: 'index.html' // may differ from host to host
+      fallback: 'fallback.html' // may differ from host to host
     })
   }
 };
